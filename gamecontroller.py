40623d1270ec758239e31c57b5c74322695b0795
Stijn Roscam <stijn.roscam@student.ap.be>
2020-05-07 12:36:54 +0200

    Editting gamecrontroller.py, adding addition guitest.py

--- gamecontroller.py
+++ gamecontroller.py
@@ -6,32 +6,112 @@ coordinates = [0,100,0,650,750,375,1400,375] #wcRol1X, wcRol1Y, wcRol2X, wcRol2Y
 canvasWidth, canvasHeight = 1500, 750
 startGame = False
 clientID = 1
+moveSpeed = 5
 
-def Broker():
-    def on_connect(client, userdata, flags, rc):
-        print("Connected with result code: "+str(rc))
-        #client.subscribe("testtopic/apLab6/raspklas")
 
-    def on_message(client, userdata, msg):
-        print("Message : "+str(msg.payload))
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code: "+str(rc))
+    #client.subscribe("testtopic/apLab6/raspklas")
 
-        #Wanneer een rpi een bericht stuurt met als msg CNCT, voer initialConnection uit
-        if str(msg.payload)[1:5] == "CNCT":
-            initialConnection(client)
+def on_message(client, userdata, msg):
+    print("Message : "+str(msg.payload))
+
+    #Wanneer een rpi een bericht stuurt met als msg CNCT, voer initialConnection uit
+    if str(msg.payload)[1:5] == "CNCT":
+        initialConnection(client)
+
+    #moveUp, met als arg het ID
+    if str(msg.payload)[7:14] == "MOVE=UP":
+        moveUp(str(msg.payload)[5:6])
+
+    #moveDown, met als arg het ID
+    elif str(msg.payload)[7:14] == "MOVE=DN":
+        moveDown(str(msg.payload)[5:6])
     
 
-    client = mqtt.Client("GameController")
-    client.on_connect=on_connect
-    client.on_message=on_message
-    client.connect("ldlcreations.ddns.net", 1883, 60)
-    client.subscribe("rpiproject/controller")
-    client.loop_start()
+client = mqtt.Client("GameController")
+client.on_connect=on_connect
+client.on_message=on_message
+client.connect("ldlcreations.ddns.net", 1883, 60)
+client.subscribe("rpiproject/controller")
+client.loop_start()
 
-    #Elke controller is gesubscribed op het topic "rpirpiproject/"+ zijn eigen client naam
-    #Naar die topic wordt een string gestuurd met daarin het clientID, zo verkrijgt iedereen een verschillend clientID 
-    def initialConnection(client):
-        client.publish("rpiproject/"+client, str(clientID))
-        clientID += 1
-        if clientID == 5:
-            client.publish("rpiproject/coord","START")
+#Elke controller is gesubscribed op het topic "rpirpiproject/"+ zijn eigen client naam
+#Naar die topic wordt een string gestuurd met daarin het clientID, zo verkrijgt iedereen een verschillend clientID 
+def initialConnection(client):
+    client.publish("rpiproject/"+client, str(clientID))
+    clientID += 1
+    if clientID == 5:
+        client.publish("rpiproject/coord","START")
+
+
+def checkCollision():
+    pass
+
+def checkBoundaries():
+    global coordinates
+    #Check coordinates in x direction, reset /stop if necessary
+    if coordinates[0] >= canvasWidth:
+        coordinates[0] = 0
+    if coordinates[2] >= canvasWidth:
+        coordinates[2] = 0
+    if coordinates[6] <= 0:
+        coordinates[6] = canvasWidth
+    #Winkelkar between boundaries of 1/3 and 2/3 of canvaswidth
+    if coordinates[4] <= canvasWidth/3:
+        coordinates[4] = canvasWidth/3
+    if coordinates[4] >= 2*canvasWidth/3:
+        coordinates[4] = 2*canvasWidth/3
+
+    #Check coordinates in y direction, reset / stop if necessary
+    if coordinates[1] <= 0:
+        coordinates[1] = 0
+    if coordinates[3] <= 0:
+        coordinates[1] = 0
+    if coordinates [7] <= 0:
+        coordinates[7] = 0
+
+    if coordinates[1] >= canvasHeight:
+        coordinates[1] = canvasHeight
+    if coordinates[3] >= canvasHeight:
+        coordinates[1] = canvasHeight
+    if coordinates [7] >= canvasHeight:
+        coordinates[7] = canvasHeight
+
+def autoMove():
+    global coordinates
+    coordinates[1] += 5
+    coordinates[3] += 5
+    coordinates[7] -= 5
+
+def moveUp(id): #Or left for winkelkar
+    global coordinates
+    if id == "1":
+        coordinates[1] -= 5
+    elif id == "2":
+        coordinates[3] -= 5
+    elif id == "3":
+        coordinates[4] -= 5
+    elif id == "4":
+        coordinates[7] -= 5
+
+def moveDown(id): #Or right for winkelkar
+    global coordinates
+    if id == "1":
+        coordinates[1] += 5
+    elif id == "2":
+        coordinates[3] += 5
+    elif id == "3":
+        coordinates[4] += 5
+    elif id == "4":
+        coordinates[7] += 5
+
+
+#Loop
+while(True):
+    time.sleep(0.5)
+    autoMove()
+    checkCollision()
+    checkBoundaries()
+    client.publish("rpiproject/coord",str(coordinates).strip('[]'))
 
--- gui2test.py
+++ gui2test.py
@@ -101,25 +101,25 @@ def GUI():
         canvas.delete(wcRol)
        # wcRol = canvas.create_image(wcRolX, wcRolY, anchor=tk.NW, image = wcrolFoto)
 
-    #Method that moves the objects automatically in correct direction
-    def moveObjects():
-        global wcRol1, wcRol2
-        kader.move(wcRol1.Image,wcRol1.Speed,1)
-        wcRol1.XCoord += wcRol1.Speed
+    def updateCoords():
+        pass
 
-        kader.move(wcRol2.Image,wcRol2.Speed,-1)
-        wcRol2.XCoord += wcRol1.Speed
+    def moveObjectsCoord():
+        global wcRol1, wcRol2, winkelKar, virus
+        kader.delete(wcRol1.Image)
+        kader.delete(wcRol2.Image)
+        kader.delete(winkelKar.Image)
+        kader.delete(virus.Image)
 
-        kader.move(virus.Image,virus.Speed,0)
-        virus.XCoord += virus.Speed
+        wcRol1.Image = kader.create_image(wcRol1.XCoord, wcRol1.YCoord, anchor=tk.NW, image = wcrolFoto)
+        wcRol2.Image = kader.create_image(wcRol2.XCoord, wcRol2.YCoord, anchor=tk.NW, image = wcrolFoto)
+        winkelKar.Image = kader.create_image(winkelKar.XCoord, winkelKar.YCoord, anchor=tk.NW, image = winkelkarFoto)
+        virus.Image = kader.create_image(virus.XCoord, virus.YCoord, anchor=tk.NW, image = virusFoto)
 
-        resetOnEnd()
-
-        venster.after(50,moveObjects) #Every 100 ms, wcrol move 5 to the right, virus 5 to left
+        venster.after(50, moveObjectsCoord)
 
 
-
-    moveObjects()
+    moveObjectsCoord()
 
     venster.mainloop()
 
@@ -131,40 +131,12 @@ def Broker():
     
     def on_message(client, userdata, msg):
         print("Message : "+str(msg.payload))
-        if str(msg.payload)[7:14] == "MOVE=UP":
-            moveUp(str(msg.payload)[5:6])
-
-        elif str(msg.payload)[7:14] == "MOVE=DN":
-            moveDown(str(msg.payload)[5:6])
-
-        elif str(msg.payload)[7:14] == "ACTI=RE":
+        if str(msg.payload)[7:14] == "ACTI=RE":
             resetObject(str(msg.payload)[5:6])
 
         elif str(msg.payload)[1:6] == "START":
             global startGame
             startGame = True
-
-    def moveUp(id):
-        global wcRol1MoveUp, wcRol2MoveUp, winkelKarMoveUp, virusMoveUp
-        if id == "1":
-            wcRol1MoveUp = True
-        elif id == "2":
-            wcRol2MoveUp = True
-        elif id == "3":
-            winkelKarMoveLeft = True
-        elif id == "4":
-            virusMoveUp = True
-
-    def moveDown(id):
-        global wcRol1MoveDown, wcRol2MoveDown, winkelKarMoveDown, virusMoveDown
-        if id == "1":
-            wcRol1MoveDown = True
-        elif id == "2":
-            wcRol2MoveDown = True
-        elif id == "3":
-            winkelKarMoveRight = True
-        elif id == "4":
-            virusMoveDown = True
 
     def resetObject(id): #Niet nodig als alles in gamecontroller word geregeld
         global wcRol1Reset, wcRol2Reset, winkelKarReset, virusReset
@@ -177,8 +149,6 @@ def Broker():
         elif id == "4":
             virusMoveReset = True
 
-
-
     client = mqtt.Client("Gui")
     client.on_connect=on_connect
     client.on_message=on_message
@@ -190,5 +160,3 @@ job1 = Thread(target=GUI)
 job2 = Thread(target=Broker)
 job1.start()
 job2.start()
-  
-
