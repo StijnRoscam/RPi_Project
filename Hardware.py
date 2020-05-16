8034f16981e94e89e58182a15b8e16d0947514e2
BePotato <55400743+BePotato@users.noreply.github.com>
2020-05-16 13:36:07 +0200

    hardware update

--- Hardware.py
+++ Hardware.py
@@ -2,21 +2,43 @@
 import RPi.GPIO as GPIO
 import time
 
-#Knop1=1
-#Knop2=2
-#LedRoodWC=3
-#LedGeelKar=4
-#LedGroenVirus=5
-ID = ""
+
+Knop1=6
+Knop2=13
+LedRoodWC=8
+LedGeelKar=25
+LedGroenVirus=24
+ID = "2"
 GPIO.setmode(GPIO.BCM)
-segments =  (27,22,9,11,0,5,6,10)
+segments =  (17,27,22,9,11,0,5,10)
 i=0
+VAR = ""
+def on_publish(client, userdata, mid):
+    print("mid: "+str(mid))
+
+def on_subscribe(client, userdata, mid, granted_qos):
+    print("Subscribed: "+str(mid)+" "+str(granted_qos))
+
+def on_message(client, userdata, msg):
+    global VAR
+    #print(str(msg.payload))
+    VAR = str(msg.payload)
+
+client = mqtt.Client()
+client.on_subscribe = on_subscribe
+client.on_message = on_message
+client.on_publish = on_publish
+client.connect("ldlcreations.ddns.net", 1883, 60)
+client.subscribe("rpiproject/coord")
+
+client.loop_start()
+
 for segment in segments:
     GPIO.setup(segment, GPIO.OUT)
     GPIO.output(segment, 0)
     print("segment setup "+ str(segment))
 
-digits = (14,15,18,17)
+digits = (14,15,18,23)
 
 for digit in digits:
     GPIO.setup(digit, GPIO.OUT)
@@ -24,40 +46,71 @@ for digit in digits:
     print("digit setup "+ str(digit))
 
 num = {' ':(0,0,0,0,0,0,0),
-    '0':(1,1,1,0,1,1,1),
-    '1':(0,0,1,0,1,0,0),
+    '0':(1,1,1,1,1,1,0),
+    '1':(0,1,1,0,0,0,0),
     '2':(1,1,0,1,1,0,1),
-    '3':(0,1,1,1,1,0,1),
-    '4':(0,0,1,1,1,1,0),
-    '5':(0,1,1,1,0,1,1),
-    '6':(1,1,1,0,0,1,0),
-    '7':(1,0,0,0,0,1,1),
+    '3':(1,1,1,1,0,0,1),
+    '4':(0,1,1,0,0,1,1),
+    '5':(1,0,1,1,0,1,1),
+    '6':(1,0,1,1,1,1,1),
+    '7':(1,1,1,0,0,0,0),
     '8':(1,1,1,1,1,1,1),
-    '9':(0,0,1,1,1,1,1)}
+    '9':(1,1,1,1,0,1,1)}
 
-#while i<1:
-#    GPIO.setup(Knop1, GPIO.IN)
-#    GPIO.setup(Knop2, GPIO.IN)
-#
-#    GPIO.setup(LedRoodWC, GPIO.OUT)
-#    GPIO.setup(LedGeelKar, GPIO.OUT)
-#    GPIO.setup(LedGroenVirus, GPIO.OUT)
-#
-#    GPIO.output(LedRoodWC, True)
-#    GPIO.output(LedGeelKar, True)
-#    GPIO.output(LedGroenVirus, True)
-#    i = i+1
-#    print("Setup complete")
+def icKnopChange(channel):
+    global Knop1
+    global Knop2
+    if channel == Knop1:
+        if GPIO.input(channel) == GPIO.LOW:
+            print("Knop1 pressed")
+    elif channel == Knop2:
+        if GPIO.input(channel) == GPIO.LOW:
+            print("Knop2 pressed")
+
+
+
+
+
+
+while i<1:
+   GPIO.setup(Knop1, GPIO.IN)
+   GPIO.setup(Knop2, GPIO.IN)
+
+   GPIO.setup(LedRoodWC, GPIO.OUT)
+   GPIO.setup(LedGeelKar, GPIO.OUT)
+   GPIO.setup(LedGroenVirus, GPIO.OUT)
+
+   if ID == "1":
+        GPIO.output(LedRoodWC, True)
+        GPIO.output(LedGeelKar, False)
+        GPIO.output(LedGroenVirus, False)
+    elif ID == "2":
+        GPIO.output(LedRoodWC, False)
+        GPIO.output(LedGeelKar, True)
+        GPIO.output(LedGroenVirus, False)
+    elif ID == "3":
+        GPIO.output(LedRoodWC, False)
+        GPIO.output(LedGeelKar, False)
+        GPIO.output(LedGroenVirus, True)
+    else :
+        GPIO.output(LedRoodWC, False)
+        GPIO.output(LedGeelKar, False)
+        GPIO.output(LedGroenVirus, False)
+
+   GPIO.add_event_detect( Knop1, GPIO.FALLING, callback=icKnopChange, bouncetime=1 )
+   GPIO.add_event_detect( Knop2, GPIO.FALLING, callback=icKnopChange, bouncetime=1 )
+   i = i+1
+   print("Setup complete")
 
 try:
     while True:
-        n = time.ctime()[11:13]+time.ctime()[14:16]
-        s = str(n).rjust(4)
+        #n = time.ctime()[11:13]+time.ctime()[14:16]
+        s = ID
         for digit in range(4):
             for loop in range(0,7):
                 GPIO.output(segments[loop], num[s[digit]][loop])
+                print(str(segments[loop]))
                 print(str(num[s[digit]]))
-                time.sleep(1)
                 if (int(time.ctime()[18:19])%2 == 0) and (digit == 1):
                     GPIO.output(10, 1)
                 else:
@@ -66,6 +119,7 @@ try:
             GPIO.output(digits[digit], 0)
             time.sleep(0.001)
             GPIO.output(digits[digit], 1)
+        
 finally:
     GPIO.cleanup()
 
